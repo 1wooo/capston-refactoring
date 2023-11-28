d03c086ff882ff25cc752e5eae439ea541371686
1wooo <dnjsdn2468@naver.com>
2023-11-29 01:34:34 +0900

    refactoring: 로그인 기능
    1. MemberServiceImpl 의존성 주입 방식 변경 (Autowired -> 생성자 주입)
    2. 회원 관련 Repository를 MemberRepo로 변경
    
    Resolves:
    Ref:
    Related to:

--- demo/src/main/java/com/example/demo/repo/MemberRepo.java
+++ demo/src/main/java/com/example/demo/repo/MemberRepo.java
@@ -7,5 +7,6 @@ import java.util.Optional;
 
 public interface MemberRepo extends JpaRepository<Member, Long> {
     boolean existsByMemberId(String memberId);
+    Optional<Member> findByMemberId(String memberId);
 //    public Optional<Member> findByMemberId(String memberid);
 }
--- demo/src/main/java/com/example/demo/service/MemberServiceImpl.java
+++ demo/src/main/java/com/example/demo/service/MemberServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.demo.service;
 
 import com.example.demo.Entity.Member;
 import com.example.demo.repo.CarNumberRepoInterface;
+import com.example.demo.repo.MemberRepo;
 import com.example.demo.repo.SpringDataJpaMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,20 +12,23 @@ import java.util.Optional;
 @Service
 public class MemberServiceImpl implements MemberServiceInterface {
 
-    @Autowired
-    private SpringDataJpaMemberRepository springDataJpaMemberRepository;
-    @Autowired
-    private CarNumberRepoInterface carNumberRepoInterface;
+    private final MemberRepo memberRepo;
+    private final CarNumberRepoInterface carNumberRepoInterface;
+
+    public MemberServiceImpl(MemberRepo memberRepo, CarNumberRepoInterface carNumberRepoInterface) {
+        this.memberRepo = memberRepo;
+        this.carNumberRepoInterface = carNumberRepoInterface;
+    }
 
     @Override
     public void register(Member member) throws Exception {
-        springDataJpaMemberRepository.save(member);
+        memberRepo.save(member);
     }
 
     @Override
     public Optional<Member> login(String MemberId, String MemberPassword) throws Exception {
 
-        Optional<Member> findMember = springDataJpaMemberRepository.findByMemberId(MemberId);
+        Optional<Member> findMember = memberRepo.findByMemberId(MemberId);
         System.out.println(findMember);
 //        Member returnMember = findMember.get(); // null인 경우에 대해 예외처리 리팩토링 필요 !!!여기
 

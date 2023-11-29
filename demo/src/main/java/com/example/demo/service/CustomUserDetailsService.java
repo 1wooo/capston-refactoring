1cc7cf471aff2c67cd0c098bd2a0c0b719237723
1wooo <dnjsdn2468@naver.com>
2023-11-30 02:33:31 +0900

    refactoring: 로그인 기능 Spring Security 적용
    1. CustomUserDetailsService 작성
    
    Resolves:
    Ref:
    Related to:

--- demo/src/main/java/com/example/demo/service/CustomUserDetailsService.java
+++ demo/src/main/java/com/example/demo/service/CustomUserDetailsService.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
+import com.example.demo.Entity.Member;
+import com.example.demo.Entity.MemberDetails;
 import com.example.demo.repo.MemberRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,12 +9,19 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
     private final MemberRepo memberRepo;
 
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
+    public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
+        Optional<Member> member = memberRepo.findByMemberId(loginId);
+        if (member.isPresent()) {
+            return new MemberDetails(member.get());
+        }else {
+            throw new UsernameNotFoundException("아이디 없음");
+        }
     }
 }
